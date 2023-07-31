345180df0db81ed5435d05d3861c77bc81e6252f
Nitish Pandey <pandeynitish3438@gmail.com>
2023-08-01 00:08:00 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        lightBlue : "#404EED",
+        blackGray : "#23272a",
+        bgcolor1 : "#F6F6F6"
+      },
+      fontFamily: {
+        'ggsans': ['ggsans-normal'],        
+      },
+    },
+  },
+  plugins: [],
+}
+
